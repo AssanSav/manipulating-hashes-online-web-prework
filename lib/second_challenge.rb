ece43ae2b3b4fdf4915a848a8260c3f07c3e53a3
assane savadogo <assane.savadogo81@gmail.com>
2019-06-04 22:54:01 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-  i = {}
+  i = []
 groceries.each do |store, data|
   data.each |attr, value|
   return i << value
